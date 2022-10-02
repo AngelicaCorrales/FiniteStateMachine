8fd9129e77ae162331f4ca6a25f7b62ee0afbd62
AngelicaCorrales <angelicacorrales2002@gmail.com>
2022-10-01 23:53:06 -0500

    icon added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import model.FSMController;
 
@@ -30,6 +31,7 @@ public class Main extends Application{
 		Scene scene= new Scene(root);
 		
 		primaryStage.setScene(scene);
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/ui/iconexe.png")));
 		primaryStage.setTitle("Finite State Machines!!");
 		primaryStage.show();
 		fsmGUI.showWelcomeWindow();
